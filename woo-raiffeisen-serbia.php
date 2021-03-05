06c9c056c9903f9c66bb61b7a7c20a118e7586ae
CheeVT <cheevt@gmail.com>
2021-03-05 17:07:00 +0100

    translation - wip

--- woo-raiffeisen-serbia.php
+++ woo-raiffeisen-serbia.php
@@ -8,6 +8,8 @@
  * Author URI: https://github.com/cheevt
  * Requires at least: x.x
  * Tested up to: x.x
+ * Domain Path: /languages
+ * Text Domain: woo-raiffeisen-serbia
  *
  * @package WordPress
  * @author cheevt
@@ -20,6 +22,12 @@ define( 'WOO_RAIFFEISEN_SERBIA_PLUGIN_NAME', plugin_basename( __FILE__ ) );
 define( 'WOO_RAIFFEISEN_SERBIA_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WOO_RAIFFEISEN_SERBIA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
+function woo_raiffeisen_serbia_init() {
+    //var_dump( WOO_RAIFFEISEN_SERBIA_PLUGIN_PATH . 'languages');
+    load_plugin_textdomain('woo-raiffeisen-serbia', false, WOO_RAIFFEISEN_SERBIA_PLUGIN_PATH . 'languages');
+}
+//add_action('init', 'woo_raiffeisen_serbia_init');
+
 // include main plugin file.
 require_once WOO_RAIFFEISEN_SERBIA_PLUGIN_PATH . 'includes/class-woo-raiffeisen-serbia-activator.php';
 register_activation_hook(__FILE__, array('WooRaiffeisenSerbiaActivator', 'plugin_activation'));
@@ -30,6 +38,8 @@ register_deactivation_hook(__FILE__, array('WooRaiffeisenSerbiaDeactivator', 'pl
 add_action('plugins_loaded', 'wc_raiffeisen_serbia_gateway_init', 11);
 function wc_raiffeisen_serbia_gateway_init()
 {
+    load_plugin_textdomain('woo-raiffeisen-serbia', false, WOO_RAIFFEISEN_SERBIA_PLUGIN_PATH . '/languages');
+
     require_once(WOO_RAIFFEISEN_SERBIA_PLUGIN_PATH . 'includes/class-woo-raiffeisen-serbia.php');
     new WooRaiffeisenSerbia();
 }
